ceb41c8d7dbc3407b68513d6821c1f67bc444d6d
Sören Ohnmeiss <soren.ohnmeiss@gmail.com>
2014-11-18 20:28:52 +0100

    translation

--- client/config.js
+++ client/config.js
@@ -1,3 +1,15 @@
 Accounts.ui.config({
     passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
+
+//Fix translation bug
+accountsUIBootstrap3.map("fr", {
+    loginFields: {
+        usernameOrEmail: "Nom d'utilisateur ou email",
+        username: "Nom d'utilisateur",
+        email: "Email",
+        password: "Mot de passe"
+    }
+});
+
+accountsUIBootstrap3.setLanguage('fr');
