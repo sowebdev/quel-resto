763b61eacdfb75dbaa2f269fef29b8dbc3142dee
Sören Ohnmeiss <soren.ohnmeiss@gmail.com>
2014-11-18 22:51:37 +0100

    App can take settings

---
+++ README.md
@@ -0,0 +1,7 @@
+#Quel resto ?
+
+##Configure the application
+
+Settings are taken from the `Meteor.settings` object. You can define settings from a JSON file :
+
+    meteor --settings settings.json
--- server/config.js
+++ server/config.js
@@ -1,3 +1,15 @@
-Accounts.config({
-    restrictCreationByEmailDomain: 'netapsys.fr'
+if (Meteor.settings.restrictCreationByEmailDomain) {
+    Accounts.config({
+        restrictCreationByEmailDomain: Meteor.settings.restrictCreationByEmailDomain
+    });
+}
+
+Meteor.startup(function(){
+    if (Restaurant.find().count() === 0) {
+        if (Meteor.settings.predefinedData) {
+            _.each(Meteor.settings.predefinedData, function(data){
+                Restaurant.insert(data);
+            });
+        }
+    }
 });
