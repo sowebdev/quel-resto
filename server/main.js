31d575a1cdcde422483eb2240897adc178a8b74a
Sören Ohnmeiss <soren.ohnmeiss@gmail.com>
2014-11-18 23:17:38 +0100

    Votes are limited in a time range

--- server/main.js
+++ server/main.js
@@ -5,11 +5,17 @@ Meteor.publish('restaurants', function(){
 Meteor.methods({
     vote: function (restaurantId) {
         if (!this.userId) {
-            throw new Meteor.Error('logged-out', 'Veuillez vous connecter');
+            throw new Meteor.Error('logged-out', 'Connecte-toi !');
         }
         var resto = Restaurant.findOne(restaurantId);
         if (!resto) {
             throw new Meteor.Error('not-found', 'Restaurant non trouvé');
+        }
+        if (moment().hour() < 9) {
+            throw new Meteor.Error('too-soon', "Bien essayé, mais c'est trop tôt ;-)");
+        }
+        if (moment().hour() > 12 || (moment().hour() == 12 && moment().minute() > 30)) {
+            throw new Meteor.Error('too-soon', "Bien essayé, mais c'est trop tard ;-)");
         }
         var updateOptions = {};
         if (_.findWhere(resto.supporters, {id: this.userId})) {
