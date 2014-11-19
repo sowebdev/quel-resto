dd920fb785e230c6d65196492fbf128666f21613
Sören Ohnmeiss <soren.ohnmeiss@gmail.com>
2014-11-19 21:48:59 +0100

    updated accounts UI package

--- client/config.js
+++ client/config.js
@@ -2,14 +2,4 @@ Accounts.ui.config({
     passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
 
-//Fix translation bug
-accountsUIBootstrap3.map("fr", {
-    loginFields: {
-        usernameOrEmail: "Nom d'utilisateur ou email",
-        username: "Nom d'utilisateur",
-        email: "Email",
-        password: "Mot de passe"
-    }
-});
-
 accountsUIBootstrap3.setLanguage('fr');
